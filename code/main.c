71826b0a49c107f6ec2c7ab707b4fc0b7479fd7b
luoyt14 <lyt18@mails.tsinghua.edu.cn>
2018-12-28 21:50:49 +0800

    parse cmdStr

--- code/main.c
+++ code/main.c
@@ -1,60 +1,93 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+#include "paramaters.h"
 
+struct cmd {
+	struct cmd* next;
+	int begin, end;
+	int argc;
+	char lredir, rredir;
+	char toFile[MAX_PATH_LENGTH], fromFile[MAX_PATH_LENGTH];
+	char *args[MAX_ARG_NUM];
+	char bgExec; // 后台执行
+};
 
-int main() {
-	char cmd[256];
-	char *args[128];
-	while (1) {
-		printf("^_^ ");
-		fflush(stdin);
-		fgets(cmd, 256, stdin);
-		int i=0;
-		while(cmd[i]!='\n')
-			++i;
-		cmd[i] = '\0';
+struct cmd cmdinfo[MAX_CMD_NUM];
+char cmdStr[MAX_CMD_LENGTH];
+int cmdNum, varNum;
 
-		args[0] = cmd;
-		for (i=0;*args[i];++i) {
-			for (args[i+1]=args[i]+1;*args[i+1];++args[i+1]){
-				if (*args[i+1] == ' '){
-					*args[i+1] = '\0';
-					++ args[i+1];
-					break;
+int parseCmds(int n) {
+	char beginCmd = 0;
+	struct cmd* head;
+
+	for (int i=0;i<=n;++i){
+		switch(cmdStr[i]) {
+			case '&':{
+				if (cmdStr[i+1] == '\n' || cmdStr[i+1] == ';') {
+					cmdStr[i] = ' ';
+					head->bgExec = 1;
 				}
 			}
+			case ';':{
+				beginCmd = 0;
+				cmdStr[i] = '\0';
+				cmdinfo[cmdNum++].end = i;
+				break;
+			}
+			case '\n':{
+				cmdStr[i] = '\0';
+				cmdinfo[cmdNum++].end = i;
+				return 0;
+			}
+			case ' ': break;
+			default: if (!beginCmd) {
+				beginCmd = 1;
+				head = cmdinfo + cmdNum;
+				cmdinfo[cmdNum].begin = i;
+			}
 		}
+	}
+}
 
-		args[i] = NULL;
+int main() {
+	char *args[128];
+	while (1) {
+		cmdNum = 0;
+		varNum = 0;
+		printf("^_^ ");
+		fflush(stdin);
+		fgets(cmdStr, MAX_CMD_LENGTH, stdin);
+		int i=0;
+		int n = strlen(cmdStr);
+		parseCmds(n);
+		
+		for (i=0;i<cmdNum;++i) {
+			struct cmd *pcmd = cmdinfo + i;
+			
+			if (!pcmd->args[0])
+				continue;
 
-		if (!args[0])
-			continue;
+			// 基本命令
+			// if (strcmp(pcmd->args[0], "cd") == 0) {
+			// 	if (pcmd->args[1])
+			// 		chdir(pcmd->args[1]);
+			// 	continue;
+			// }
+			if (strcmp(pcmd->args[0], "pwd") == 0) {
+				char wd[4096];
+				puts(getcwd(wd, 4096));
+				continue;
+			}
+			if (strcmp(pcmd->args[0], "exit") == 0) {
+				return 0;
+			}
 
-		// 基本命令
-		if (strcmp(args[0], "cd") == 0) {
-			if (args[1])
-				chdir(args[1]);
-			continue;
+			// 较高级命令
+			pid_t pid = fork();
+			if (pid == 0) {
+				execvp(pcmd->args[0], args);
+				return 255;
+			}
+			wait(NULL);
 		}
-		if (strcmp(args[0], "pwd") == 0) {
-			char wd[4096];
-			puts(getcwd(wd, 4096));
-			continue;
-		}
-		if (strcmp(args[0], "exit") == 0) {
-			return 0;
-		}
-
-		// 较高级命令
-		pid_t pid = fork();
-		if (pid == 0) {
-			execvp(args[0], args);
-			return 255;
-		}
-
-		wait(NULL);
+		
 	}
 }
---
+++ code/paramaters.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef __PARAMATERS_H
+#define __PARAMATERS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define MAX_CMD_LENGTH 255
+#define MAX_PATH_LENGTH 255
+#define MAX_BUF_SIZE 4096
+#define MAX_ARG_NUM 50
+#define MAX_VAR_NUM 50
+#define MAX_CMD_NUM 10
+#define MAX_VAR_LENGTH 500
+
+#endif
